ea2357e6edec66ad2c4ebbaf2d7a1994be07af3f
AAlMa <a.almakarem@hotmail.com>
2024-03-13 13:04:28 +0300

    request success but the picture doesn't change

--- app/src/main/java/com/example/cleanhiltapp/common/Constants.kt
+++ app/src/main/java/com/example/cleanhiltapp/common/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val CoinPaprika_BASE_URL = "https://api.coinpaprika.com/"
     const val PARAM_COIN_ID = "coinId"
     const val ChatGPT_EndPoint = "https://api.openai.com/v1/"
-    const val ChatGPT_Token = "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"
+    const val ChatGPT_Token = ""
 }
